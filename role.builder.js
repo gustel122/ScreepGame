63b69a4b85f18d69495484ac50a2eb730b199203
Justin Matthias <gustel122@pop-os.localdomain>
2021-08-29 05:55:04 +0200

    Builder angepasst

--- role.builder.js
+++ role.builder.js
@@ -21,6 +21,21 @@ const roleBuilder = {
             if (creep.build(buildings) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(buildings);
                 creep.say('Baue!');
+            } else {
+                const targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: object => object.hits < object.hitsMax
+                });
+                
+                targets.sort((a,b) => a.hits - b.hits);
+    
+                if(targets.length > 0) {
+                    if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0]);
+                        creep.say('Repariere!');
+                    }
+ 
+                    
+                }
             }
         
         } 
